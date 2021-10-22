bea16119bf933fb35dd15f7f0c9ee574ed21097f
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2021-10-22 11:48:23 +0100

    Added Get version of Ticket

---
+++ AutoTask.Psa.Api.Test/TicketTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoTask.Psa.Api.Test.Config;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AutoTask.Psa.Api.Test
+{
+	public class TicketTests : TestBase
+	{
+		public TicketTests(
+			ITestOutputHelper testOutputHelper,
+			IOptions<AppSettings> options) : base(testOutputHelper, options)
+		{
+		}
+
+		[Fact]
+		public async void QueryAsync_WithString_Succeeds()
+		{
+			var response = await AutoTaskClient
+				.Tickets
+				.QueryAsync("{\"filter\": [ {\"field\": \"createDate\", \"op\": \"gte\", \"value\": \"2021-09-01\"}]}")
+				.ConfigureAwait(false);
+
+			response.Should().NotBeNull(because: "a valid request should return a response object");
+			response.Items.Should().NotBeEmpty(because: "there should be at least one ticket created in the last 24 hours");
+		}
+	}
+}
--- AutoTask.Psa.Api/Interfaces/TicketsApi.cs
+++ AutoTask.Psa.Api/Interfaces/TicketsApi.cs
@@ -35,6 +35,15 @@ namespace AutoTask.Psa.Api.Interfaces
 		Task<QueryActionResult<TicketModel>> QueryAsync([Body] QueryModel queryModel);
 
 		/// <summary>
+		/// Performs a query using a search JSON filter
+		/// </summary>
+		/// <exception cref="Exceptions.ApiException">Thrown when fails to make API call</exception>
+		/// <param name="search">JSON filter</param>
+		/// <returns>QueryActionResult of TicketModel</returns>
+		[Get("/V1.0/Tickets/query")]
+		Task<QueryActionResult<TicketModel>> QueryAsync(string search);
+
+		/// <summary>
 		///
 		/// </summary>
 		/// <exception cref="Exceptions.ApiException">Thrown when fails to make API call</exception>
